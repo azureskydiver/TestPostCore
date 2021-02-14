a8c1c98077fbccee56034bc971663d870634f381
Ants Francisco <ants+github@recombulator.net>
2021-02-13 19:46:13 -0500

    Add project files.

---
+++ TestPostCore/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestPostCore.Controllers
+{
+    [Route("Search")]
+    public class SearchController : Controller
+    {
+        [HttpPost]
+        public IActionResult Search(string term, DateTime magic)
+        {
+            return RedirectToAction("Search", new { term = term });
+        }
+
+        [Route("Search/{term}")]
+        public IActionResult Search(string term)
+        {
+            ViewBag.Term = term;
+            return View();
+        }
+    }
+}
